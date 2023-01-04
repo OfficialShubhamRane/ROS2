0fd4b46056441641e607ba6f741806692394b4fd
Shubham Rane <official.shubhamrane@gmail.com>
2023-01-03 20:35:32 -0600

    changing udemy dir 2

---
+++ udemy_ros2_pkg/scripts/publisher.py
@@ -0,0 +1,31 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+class HelloWorldPublisher(Node):
+    def __init__(self):
+        super().__init__("hello_world_pub_node")
+        self.pub = self.create_publisher(String, "hello_world", 10)
+        self.timer = self.create_timer(2, self.hello_world_callback)
+
+    def hello_world_callback(self):
+        msg = String()
+        msg.data = "Hello world"
+        self.pub.publish(msg)
+
+
+def main():
+    rclpy.init()
+
+    hello_world_pub = HelloWorldPublisher()
+    print("Publisher node running...")
+
+    try:
+        rclpy.spin(hello_world_pub)
+    except KeyboardInterrupt:
+        hello_world_pub.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
