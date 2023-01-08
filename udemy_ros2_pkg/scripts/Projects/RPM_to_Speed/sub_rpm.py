976e4e4873a440ccd362fea3be9a77f9da12fded
Shubham Rane <official.shubhamrane@gmail.com>
2023-01-08 11:56:43 -0600

    use parameters to change wheel radius

--- udemy_ros2_pkg/scripts/Projects/RPM_to_Speed/sub_rpm.py
+++ udemy_ros2_pkg/scripts/Projects/RPM_to_Speed/sub_rpm.py
@@ -2,18 +2,20 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
 
-WHEEL_RADIUS = 12.5 / 100 # centimeter to meters
+WHEEL_RADIUS_DEFAULT = 12.5 / 100 # centimeter to meters
 
 class Sub_rpm_node(Node):
     
     def __init__(self):
         super().__init__("sub_rpm")
+        self.declare_parameter("wheel_radius", WHEEL_RADIUS_DEFAULT)
         self.sub = self.create_subscription(Float32, "topic_rpm", self.rpm_processor_callback, 10)
         self.pub = self.create_publisher(Float32, "topic_speed", 10)
 
 
     def rpm_processor_callback(self, rpm_msg):
-        speed = rpm_msg.data * WHEEL_RADIUS * 2 * 3.14159 / 60 # speed in m/s
+        wheel_radius_param = self.get_parameter("wheel_radius").get_parameter_value().double_value
+        speed = rpm_msg.data * wheel_radius_param * 2 * 3.14159 / 60 # speed in m/s
 
         speed_msg = Float32()
         speed_msg.data = float(speed)
